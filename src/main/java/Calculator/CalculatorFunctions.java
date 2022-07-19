22fb3b0eeaecfa7b686d4bf57a35a2683cf442eb
IshavJ <64170882+IshavJ@users.noreply.github.com>
2022-07-19 15:47:24 +0530

    Add files via upload

---
+++ src/main/java/Calculator/CalculatorFunctions.java
@@ -0,0 +1,31 @@
+package Calculator;
+
+public class CalculatorFunctions {
+    public static void add(int a,int b){
+        System.out.println(a+b);
+    }
+    public static void add(float a,float b){
+        System.out.println(a+b);
+    }
+
+    public static void sub(int a,int b){
+        System.out.println(a-b);
+    }
+    public static void sub(float a,float b){
+        System.out.println(a-b);
+    }
+
+    public static void div(int a,int b){
+        System.out.println(a/b);
+    }
+    public static void div(float a,float b){
+        System.out.println(a/b);
+    }
+
+    public static void multi(int a,int b){
+        System.out.println(a*b);
+    }
+    public static void multi(float a,float b){
+        System.out.println(a*b);
+    }
+}
