b9f0d5776113e1100bc65e478ae7a6394c76d690
IshavJ <64170882+IshavJ@users.noreply.github.com>
2022-07-31 15:55:51 +0530

    Add files via upload
    Updated Month Function

--- src/main/java/Month/MonthswithClass.java
+++ src/main/java/Month/MonthswithClass.java
@@ -1,11 +1,26 @@
 import java.time.Month;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class MonthswithClass {
+
+    public static void output(String month) {
+        String o = month.substring(0, 1);
+        String p = month.substring(1);
+        System.out.println(o + p.toLowerCase(Locale.ROOT));
+    }
+
     public static void main(String[] args) {
-        System.out.println("Pass Caps Month name");
-        Month mon = Month.valueOf(new Scanner(System.in).next());
+        System.out.println("Pass Month name ");
+        Month mon = Month.valueOf(new Scanner(System.in).next().toUpperCase(Locale.ROOT));
+        System.out.println("Enter number of month(s) for addition ");
         int addition = mon.getValue() + new Scanner(System.in).nextInt();
-        System.out.println((addition <=12)?Month.of(addition).name():Month.of(addition - 12).name());
+
+        //System.out.println((addition <= 12) ?  Month.of(addition).name() : Month.of(addition - 12).name());
+
+        if(addition <= 12)
+            output(Month.of(addition).name());
+        else
+            output(Month.of(addition - 12).name());
     }
 }
